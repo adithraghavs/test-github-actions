6f7ee4ea658746aca115c5428cce19bac0998c7f
Adith Raghav <65845669+adithraghavs@users.noreply.github.com>
2022-02-21 07:54:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.run(["git", "show", "HEAD~2:test.xml"])
+subprocess.run(["git", "diff", "12495f9d27b5a5eee11814e5c34dda490c2d314b", "6d830710625c358ad6c0ee6ef0d07bc60b47aa36"])
